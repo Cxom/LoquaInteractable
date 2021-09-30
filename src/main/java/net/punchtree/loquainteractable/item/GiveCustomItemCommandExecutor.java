0bd01ab000df827e47aa6a0fabd79467a0700509
Cxom <cxomtdoh@yahoo.com>
2021-09-30 08:46:17 -0400

    add basic givecustom permission

--- src/main/java/net/punchtree/loquainteractable/item/GiveCustomItemCommandExecutor.java
+++ src/main/java/net/punchtree/loquainteractable/item/GiveCustomItemCommandExecutor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,6 +14,8 @@ import org.bukkit.util.StringUtil;
 
 public class GiveCustomItemCommandExecutor implements CommandExecutor, TabCompleter {
 
+	private static final String GIVECUSTOM_PERMISSION = "givecustom";
+	
 	public final Iterable<String> customItemKeys = Arrays.asList("coffee");
 	
 	// This will be abstracted out into taking in a custom item registry
@@ -24,6 +27,10 @@ public class GiveCustomItemCommandExecutor implements CommandExecutor, TabComple
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
 		if ( ! (sender instanceof Player player)) return true;
+		if ( ! (sender.hasPermission(GIVECUSTOM_PERMISSION))) {
+			// TODO this is a default that should be abstracted, maybe at the server level
+			player.sendMessage(ChatColor.RED + "Either this command doesn't exist or you don't have permission to perform this command.");
+		}
 		
 		player.getInventory().addItem(DrinkItems.COFFEE.clone());
 		
